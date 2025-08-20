436fee81545be90e7e1194a6b6fee8f80120ef92
OrYizhakian <Ytzhakianor@gmail.com>
2025-08-20 20:24:59 +0300

    Fifth time adding - changed 3 times and hello.java changed few times more

--- OrYizhakian.java
+++ OrYizhakian.java
@@ -1,7 +1,7 @@
 public class OrYizhakian{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Or Yizhakian 2"+i);
+			System.out.println("Or Yizhakian 3 "+i);
 		}
 	}
 }
